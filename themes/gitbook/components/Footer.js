10e449822c4a4aae2c103726a2f9552fdb41f7db
codemaster1024 <1787806000@qq.com>
2024-09-07 13:26:19 +0800

    修改备案地址

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -43,7 +43,7 @@ const Footer = ({ siteInfo }) => {
       {siteConfig('BEI_AN') && (
         <>
           <i className='fas fa-shield-alt' />{' '}
-          <a href='https://beian.miit.gov.cn/' className='mr-2'>
+          <a href='https://icp.gov.moe/?keyword=20245588' className='mr-2'>
             {siteConfig('BEI_AN')}
           </a>
           <br />
